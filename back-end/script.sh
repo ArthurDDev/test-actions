311bf0e566c8890ed0a5047190311643bbc95046
ArthurDDev <ArthurDDev@gmail.com>
2024-10-23 22:01:46 -0300

    [add] docker compose

---
+++ back-end/script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "=================== STARTING BACK-END ==================="
+
+ln -s /etc/nginx/sites-available/projeto.config-front /etc/nginx/sites-enabled/projeto.config-front
+ln -s /etc/nginx/sites-available/projeto.config-back /etc/nginx/sites-enabled/projeto.config-back
+rm /etc/nginx/sites-enabled/default
+cp .env.example .env
+
+composer install
+php artisan key:generate
+php artisan migrate:fresh
+service nginx start
+
+echo "=================== BACK-END CONFIG ENDED ==================="
+
+sleep 10
+
+echo "=================== NGINX STATUS ==================="
+service nginx status
+echo "==================================================="
+
+tail -f /dev/null # Garante que o container nao acaba
---
+++ front-end/script.sh
@@ -0,0 +1,7 @@
+pm2 start script.json
+sleep 10
+# echo "=================== PM2 STOPPED ==================="
+pm2 status
+# echo "==================================================="
+
+tail -f /dev/null # Garante que o container nao acaba
