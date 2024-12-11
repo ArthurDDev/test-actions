bea0d6f8583e10570e9ad673dd0df48de100e0be
arthurddev <arthurddev@gmail.com>
2024-12-11 14:54:48 -0300

    aleluia deus acho que deu certo

--- back-end/script.sh
+++ back-end/script.sh
@@ -2,9 +2,9 @@
 
 echo "=================== STARTING BACK-END ==================="
 
-RUN add-apt-repository -y ppa:ondrej/php > /dev/null
-RUN apt-get install -y php7.4
-RUN apt-get install -y php7.4-curl php7.4-cli php7.4-dev php7.4-gd php7.4-intl php7.4-json php7.4-mysql php7.4-opcache php7.4-bcmath php7.4-mbstring php7.4-soap
+add-apt-repository -y ppa:ondrej/php > /dev/null
+apt-get install -y php7.4
+apt-get install -y php7.4-curl php7.4-cli php7.4-dev php7.4-gd php7.4-intl php7.4-json php7.4-mysql php7.4-opcache php7.4-bcmath php7.4-mbstring php7.4-soap
 
 ln -s /etc/nginx/sites-available/projeto.config-front /etc/nginx/sites-enabled/projeto.config-front
 ln -s /etc/nginx/sites-available/projeto.config-back /etc/nginx/sites-enabled/projeto.config-back
@@ -15,6 +15,8 @@ composer install
 php artisan key:generate
 php artisan migrate:fresh
 service nginx start
+
+chown -R www-data:www-data storage bootstrap/cache #Permite que o servidor de fato acesse o armazenamento
 
 echo "=================== BACK-END CONFIG ENDED ==================="
 
